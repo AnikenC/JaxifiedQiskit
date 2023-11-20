50eafd52bdf4a98abe16bd77c213c5dcd77221d3
AnikenC <quantac47@gmail.com>
2023-11-20 22:18:43 +0000

    updated everything

--- library/new_sims.py
+++ library/new_sims.py
@@ -12,16 +12,62 @@ import chex
 
 from typing import Optional, Union
 
+from library.utils import PauliToQuditOperator
 
-def PauliToQuditOperator(inp_str: str, qudit_dim_size: Optional[int] = 4):
-    word_list = list(inp_str)
-    qudit_op_list = []
-    for word in word_list:
-        qubit_op = base_gates_dict[word]
-        qud_op = np.identity(qudit_dim_size, dtype=np.complex64)
-        qud_op[:2, :2] = qubit_op
-        qudit_op_list.append(qud_op)
-    complete_op = qudit_op_list[0]
-    for i in range(1, len(qudit_op_list)):
-        complete_op = np.kron(complete_op, qudit_op_list[i])
-    return complete_op
+
+class JaxedSolver:
+    def __init__(
+        self, schedule_func, solver, dt, carrier_freqs, ham_chans, t_span, rtol, atol
+    ):
+        super().__init__()
+        self.schedule_func = schedule_func
+        self.solver = solver
+        self.dt = dt
+        self.carrier_freqs = carrier_freqs
+        self.ham_chans = ham_chans
+        self.t_span = t_span
+        self.rtol = rtol
+        self.atol = atol
+        self.fast_batched_sim = jit(vmap(self.run_sim))
+
+    def run_sim(self, y0, obs, params):
+        sched = self.schedule_func(params)
+
+        converter = InstructionToSignals(
+            self.dt, carriers=self.carrier_freqs, channels=self.ham_chans
+        )
+
+        signals = converter.get_signals(sched)
+
+        results = self.solver.solve(
+            t_span=self.t_span,
+            y0=y0 / jnp.linalg.norm(y0),
+            t_eval=self.t_span,
+            signals=signals,
+            rtol=self.rtol,
+            atol=self.atol,
+            convert_results=False,
+            method="jax_odeint",
+        )
+
+        state_vec = results.y.data[-1]
+        state_vec = state_vec / jnp.linalg.norm(state_vec)
+        new_vec = obs @ state_vec
+        probs_vec = jnp.abs(new_vec) ** 2
+        probs_vec = jnp.clip(probs_vec, a_min=0.0, a_max=1.0)
+
+        # Shots instead of probabilities
+
+        return probs_vec
+
+    def estimate2(self, batch_y0, batch_params, batch_obs_str):
+        batch_obs = jnp.zeros(
+            (batch_y0.shape[0], batch_y0.shape[1], batch_y0.shape[1]),
+            dtype=jnp.complex64,
+        )
+        num_qubits = len(batch_obs_str[0])
+        for i, b_str in enumerate(batch_obs_str):
+            batch_obs = batch_obs.at[i].set(
+                PauliToQuditOperator(b_str, int(batch_y0.shape[1] ** (1 / num_qubits)))
+            )
+        return self.fast_batched_sim(batch_y0, batch_obs, batch_params)
--- library/utils.py
+++ library/utils.py
@@ -41,3 +41,64 @@ def PauliToQuditOperator(inp_str: str, qudit_dim_size: Optional[int] = 4):
     for i in range(1, len(qudit_op_list)):
         complete_op = np.kron(complete_op, qudit_op_list[i])
     return complete_op
+
+
+class TwoQuditHamiltonian:
+    def __init__(
+        self,
+        qudit_dim,
+    ):
+        super().__init__()
+
+        self.qudit_dim = qudit_dim
+
+        v0 = 4.86e9
+        anharm0 = -0.32e9
+        r0 = 0.22e9
+
+        v1 = 4.97e9
+        anharm1 = -0.32e9
+        r1 = 0.26e9
+
+        J = 0.002e9
+
+        a = np.diag(np.sqrt(np.arange(1, qudit_dim)), 1)
+        adag = np.diag(np.sqrt(np.arange(1, qudit_dim)), -1)
+        N = np.diag(np.arange(qudit_dim))
+
+        ident = np.eye(qudit_dim, dtype=complex)
+        full_ident = np.eye(qudit_dim**2, dtype=complex)
+
+        N0 = np.kron(ident, N)
+        N1 = np.kron(N, ident)
+
+        a0 = np.kron(ident, a)
+        a1 = np.kron(a, ident)
+
+        a0dag = np.kron(ident, adag)
+        a1dag = np.kron(adag, ident)
+
+        static_ham0 = 2 * np.pi * v0 * N0 + np.pi * anharm0 * N0 * (N0 - full_ident)
+        static_ham1 = 2 * np.pi * v1 * N1 + np.pi * anharm1 * N1 * (N1 - full_ident)
+
+        static_ham_full = (
+            static_ham0 + static_ham1 + 2 * np.pi * J * ((a0 + a0dag) @ (a1 + a1dag))
+        )
+
+        drive_op0 = 2 * np.pi * r0 * (a0 + a0dag)
+        drive_op1 = 2 * np.pi * r1 * (a1 + a1dag)
+
+        dt = 1 / 4.5e9
+
+        ham_ops = [drive_op0, drive_op1, drive_op0, drive_op1]
+        ham_chans = ["d0", "d1", "u0", "u1"]
+        chan_freqs = {"d0": v0, "d1": v1, "u0": v1, "u1": v0}
+
+        self.solver = Solver(
+            static_hamiltonian=static_ham_full,
+            hamiltonian_operators=ham_ops,
+            rotating_frame=static_ham_full,
+            hamiltonian_channels=ham_chans,
+            channel_carrier_freqs=chan_freqs,
+            dt=dt,
+        )
